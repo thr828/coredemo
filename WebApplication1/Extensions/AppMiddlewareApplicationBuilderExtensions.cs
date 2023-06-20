574b1f4b2ea366d459073aaaba979997001db9c8
thr828 <18550202789@126.com>
2023-06-20 11:10:37 +0800

    基于工厂的中间件

---
+++ WebApplication1/Extensions/AppMiddlewareApplicationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication1.MiddleWare;
+
+namespace WebApplication1.Extensions
+{
+    public static class AppMiddlewareApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder MyUse(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<MyMiddleware>();
+        }
+
+        public static IApplicationBuilder YouUse(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<YouMiddleWarecs>();
+        }
+    }
+}
---
+++ WebApplication1/MiddleWare/YouMiddleWarecs.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.MiddleWare
+{
+    public class YouMiddleWarecs:IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            // 下一个中间件处理之前的操作
+            Console.WriteLine("YourMiddleware Begin");
+            await next(context);
+            // 下一个中间件处理完成后的操作
+            Console.WriteLine("YourMiddleware End");
+        }
+    }
+}
